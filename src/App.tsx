11609b87c58443665597fea3cc881ff9269e4a74
Josh Byster <joshb1050@gmail.com>
2020-12-24 00:35:53 -0600

    Add ServiceWorker wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
   IonIcon,
@@ -7,35 +7,37 @@ import {
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
-  IonTabs
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import HomePage from './pages/Home';
-import StatsPanel from './pages/StatsPanel';
-import Tab3 from './pages/Tab3';
+  IonTabs,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { ellipse, square, triangle } from "ionicons/icons";
+import HomePage from "./pages/Home";
+import StatsPanel from "./pages/StatsPanel";
+import Tab3 from "./pages/Tab3";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+import ServiceWorkerWrapper from "./components/ServiceWorkerWrapper";
 
 const App: React.FC = () => (
   <IonApp>
+    <ServiceWorkerWrapper />
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
---
+++ src/components/ServiceWorkerWrapper.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useEffect } from "react";
+import { IonAlert } from "@ionic/react";
+import * as serviceWorker from "../serviceWorker";
+
+const ServiceWorkerWrapper: React.FC = () => {
+  const [showReload, setShowReload] = React.useState(false);
+  const [
+    waitingWorker,
+    setWaitingWorker,
+  ] = React.useState<ServiceWorker | null>(null);
+
+  const onSWUpdate = (registration: ServiceWorkerRegistration) => {
+    setShowReload(true);
+    setWaitingWorker(registration.waiting);
+  };
+
+  useEffect(() => {
+    serviceWorker.register({ onUpdate: onSWUpdate });
+    console.log("Registered ServiceWorker!");
+  }, []);
+
+  const reloadPage = () => {
+    waitingWorker?.postMessage({ type: "SKIP_WAITING" });
+    setShowReload(false);
+    window.location.reload(true);
+  };
+  return (
+    <IonAlert
+      isOpen={showReload}
+      onDidDismiss={() => {
+        reloadPage();
+      }}
+      header={"Update"}
+      message={`A new version is available!`}
+      buttons={["OK"]}
+    />
+  );
+};
+
+export default ServiceWorkerWrapper;
