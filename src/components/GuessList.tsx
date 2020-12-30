6691af110623a533cde02aa96e65abeaedeecdc3
Josh Byster <joshb1050@gmail.com>
2020-12-29 22:36:33 -0600

    Make guess list grow from low to high

--- src/components/GuessList.tsx
+++ src/components/GuessList.tsx
@@ -1,6 +1,7 @@
 import { IonList } from "@ionic/react";
+import { action, autorun, reaction } from "mobx";
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { GameState } from "../store/GameState";
 import "./GuessList.css";
 import { GuessListItem } from "./GuessListItem";
@@ -16,18 +17,25 @@ interface Guess {
   incorrect: number;
 }
 const GuessList: React.FC<ListProps> = observer(({ state }) => {
+  const messagesEndRef = useRef<HTMLInputElement>(null);
+
+  const scrollIntoView = () => {
+    if (null !== messagesEndRef.current) {
+      messagesEndRef.current!.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  };
+
+  useEffect(() => reaction(() => state.guesses.length, scrollIntoView), []);
+
   return (
     <IonList>
-      {state.guesses
-        .slice()
-        .reverse()
-        .map((item, idx) => (
-          <GuessListItem
-            item={item}
-            idx={state.guesses.length - idx}
-            key={idx}
-          ></GuessListItem>
-        ))}
+      {state.guesses.slice().map((item, idx) => (
+        <GuessListItem item={item} idx={idx + 1} key={idx}></GuessListItem>
+      ))}
+      <div ref={messagesEndRef} />
     </IonList>
   );
 });
