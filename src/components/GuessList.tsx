8b8b0da328b9dd53f649608899bbc543568df6f0
Josh Byster <joshb1050@gmail.com>
2021-01-02 01:26:12 -0600

    Change order of guess list appearing

--- src/components/GuessList.tsx
+++ src/components/GuessList.tsx
@@ -1,7 +1,6 @@
 import { IonList } from "@ionic/react";
-import { reaction } from "mobx";
 import { observer } from "mobx-react-lite";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { GameState } from "../store/GameState";
 import "./GuessList.css";
 import { GuessListItem } from "./GuessListItem";
@@ -17,27 +16,18 @@ interface Guess {
   incorrect: number;
 }
 const GuessList: React.FC<ListProps> = observer(({ state }) => {
-  const messagesEndRef = useRef<HTMLInputElement>(null);
-
-  const scrollIntoView = () => {
-    if (null !== messagesEndRef.current) {
-      messagesEndRef.current!.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest",
-      });
-    }
-  };
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => reaction(() => state.guesses.length, scrollIntoView), []);
-
   return (
     <IonList>
-      {state.guesses.slice().map((item, idx) => (
-        <GuessListItem item={item} idx={idx + 1} key={idx}></GuessListItem>
-      ))}
-      <div ref={messagesEndRef} id="scroll-fix" />
+      {state.guesses
+        .slice()
+        .reverse()
+        .map((item, idx) => (
+          <GuessListItem
+            item={item}
+            idx={state.guesses.length - idx}
+            key={idx}
+          ></GuessListItem>
+        ))}
     </IonList>
   );
 });
